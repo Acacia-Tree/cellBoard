5cab502d49cd5f2ff1d97f809ec9e44b3e2d0dd4
Acacia-Tree <59014594+Acacia-Tree@users.noreply.github.com>
2020-12-14 21:25:53 +0300

    changed the order of the functions for readability

--- cellBoard.js
+++ cellBoard.js
@@ -1,72 +1,6 @@
-/* Дана доска размером M × N клеток. 
-Клетка может находиться в одном из двух состояний: 1 — живая, 0 — мёртвая. 
-Каждая клетка взаимодействует с восемью соседями. Правила таковы:
-
-        1) Живая клетка, у которой меньше двух живых соседей, погибает.
-
-        2) Живая клетка, у которой два или три живых соседа, выживает.
-
-        3) Живая клетка, у которой больше трёх живых соседей, погибает.
-
-        4) Мёртвая клетка, у которой три живых соседа, возрождается.
-
-Напишите программу, которая будет: 
-+ случайным образом генерить стартовое состояние;
-+- уметь получать его из файла (способ выбирается через параметры запуска в консоли);
-+— каждую секунду выводить в консоль новое состояние доски
-*/
 let board = new Array();
 let m, n;
 
-function updateBoard(board, m, n) {
-  let newBoard = new Array();
-  let aliveCells;
-  for (i = 0; i < m; i++) {
-    newBoard[i] = new Array(n ).fill("0");
-    for (j = 0; j < n; j++) {
-
-      if ((j == 0) || (i == 0) || (j == (n - 1)) || (i == (m - 1)) ) {
-        board[i][j] = "0";// Добавить padding со всех сторон, чтобы экономить на дополнительные проверки(существует ли там элемент)
-//padding будет мертвая клетка, так как она ни на что не влияет
-//но обрабатывать(оживлять, умертвлять) клетки будем только вне этих экстра элементов, экстра элементы не будет в центре внимания и не будут меняться
-//выводим массив без padding
-      } else {
-        aliveCells = checkSurroundingCells(board, i, j);
-        if (board[i][j] == "1") {
-          if (( aliveCells < 2) || (aliveCells > 3)) {
-            newBoard[i][j] = "0";
-          } else {
-            newBoard[i][j] = "1";
-          }
-        } else {
-          if (aliveCells == "3") {
-            newBoard[i][j] = "1";
-          } else {
-            newBoard[i][j] = "0";
-          }
-        }
-      }
-
-    }
-  }
-  return newBoard;
-}
-
-function checkSurroundingCells(board, iCell, jCell) {//на выходе количество живых клеток вокруг
-  let aliveCells = 0;
-
-  let di = [-1, -1, -1, 0, 1, 1, 1, 0];
-  let dj = [-1,  0,  1, 1, 1, 0,-1,-1];// смещения, соответствующие соседям ячейки
-
-  for (let n = 0; n < 8; n++) {
-    let i = iCell + di[n];
-    let j = jCell + dj[n];
-    if (board[i][j] == "1") {
-      aliveCells++;
-    }
-  }
-  return aliveCells;
-}
 function boardGenerator(m, n) {
   let max = 2 ** (m * n - 1);
   let min = (2 ** (m * n));
@@ -80,6 +14,10 @@ function boardGenerator(m, n) {
     for (let j = 0; j < (n + 2); j++) {//n+2 for padding
       if ((j == 0) || (i == 0) || (j == (n + 1)) || (i == (m + 1)) ) {
         board[i][j] = "0"; //added padding
+        // Добавила padding со всех сторон, чтобы экономить на дополнительные проверки(существует ли там элемент)
+//padding будет мертвая клетка, так как она ни на что не влияет
+//но обрабатывать(оживлять, умертвлять) клетки будем только вне этих экстра элементов, экстра элементы не будет в центре внимания и не будут меняться
+//выводим массив без padding
       } else {
         board[i][j] = boardNumber[0];
         boardNumber = boardNumber.slice(1, boardNumber.length);
@@ -130,11 +68,51 @@ function showBoard(boardArray, m, n) {
   }
 }
 
+function checkSurroundingCells(board, iCell, jCell) {//на выходе количество живых клеток вокруг
+  let aliveCells = 0;
 
-function startUpdateLoop() {
-  board = updateBoard(board, m + 2, n + 2);
-  console.log();
-  showBoard(board, m + 2, n + 2); //+2 for padding
+  let di = [-1, -1, -1, 0, 1, 1, 1, 0];
+  let dj = [-1,  0,  1, 1, 1, 0,-1,-1];// смещения, соответствующие соседям ячейки
+
+  for (let n = 0; n < 8; n++) {
+    let i = iCell + di[n];
+    let j = jCell + dj[n];
+    if (board[i][j] == "1") {
+      aliveCells++;
+    }
+  }
+  return aliveCells;
+}
+
+function updateBoard(board, m, n) {
+  let newBoard = new Array();
+  let aliveCells;
+  for (i = 0; i < m; i++) {
+    newBoard[i] = new Array(n ).fill("0");
+    for (j = 0; j < n; j++) {
+
+      if ((j == 0) || (i == 0) || (j == (n - 1)) || (i == (m - 1)) ) {
+        board[i][j] = "0";
+      } else {
+        aliveCells = checkSurroundingCells(board, i, j);
+        if (board[i][j] == "1") {
+          if (( aliveCells < 2) || (aliveCells > 3)) {
+            newBoard[i][j] = "0";
+          } else {
+            newBoard[i][j] = "1";
+          }
+        } else {
+          if (aliveCells == "3") {
+            newBoard[i][j] = "1";
+          } else {
+            newBoard[i][j] = "0";
+          }
+        }
+      }
+
+    }
+  }
+  return newBoard;
 }
 
 function setupUpdateLoop() {
@@ -161,6 +139,12 @@ function setupUpdateLoop() {
   process.stdin.setRawMode(true);
   process.stdin.resume();
   process.stdin.on('data', process.exit.bind(process, 0));
+}
+
+function startUpdateLoop() {
+  board = updateBoard(board, m + 2, n + 2);
+  console.log();
+  showBoard(board, m + 2, n + 2); //+2 for padding
 }
 
 setupUpdateLoop();
